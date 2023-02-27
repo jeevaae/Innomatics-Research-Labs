b1ddc03c0ea94dd2a0de00d9d7e739f4fe960dda
A E JEEVA <107612269+jeevaae@users.noreply.github.com>
2023-02-27 21:39:30 +0530

    Add files via upload

---
+++ About me .py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+st.title('Welcome to My App')
+
+st.subheader('Hello, My name is :blue[A E Jeeva]')
+st.subheader(':red[Data Science Intern](Innomatics Resarch Labs)')
+
+
+st.subheader('This web app will give you basic Data visualization about :red["Iris Dataset"] and :blue["Titanic Dataset"]') 
+st.write("Checkout my Github and LinkedIn")
+st.markdown('[GitHub ](https://github.com/jeevaae/Innomatics-Research-Labs)')
+st.markdown('[LinkedIn ](https://www.linkedin.com/in/jeevaae/)')
---
+++ pages/2_Dashboard_Titanic.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from matplotlib import image
+import plotly.express as px
+import os
+import pandas as pd
+
+st.title("Dashboard - Titanic Data")
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "titanic.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "titanic.csv")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+
+st.write('Titanic Dataset')
+df1= df.head()
+st.dataframe(df1)
+
+sex = st.selectbox("Select the sex:", df['sex'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['sex'] == sex], x="age")
+col1.plotly_chart(fig_1,use_container_width=True )
+
+
+survived = st.selectbox("people survided:", df['alive'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['alive'] == survived], x="age")
+col1.plotly_chart(fig_1,use_container_width=True )
+
+
